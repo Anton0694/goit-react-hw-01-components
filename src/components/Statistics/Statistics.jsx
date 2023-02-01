2d53c5650d677eaa786d72c1c150c0e57d3c5e9a
Anton0694 <91780319+Anton0694@users.noreply.github.com>
2023-02-01 23:07:15 +0200

    +

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -3,11 +3,11 @@ import PropTypes from 'prop-types';
 
 
 
-export const Statistics = ({stats}) => {
+export const Statistics = ({title, stats}) => {
     return (
         
             <section className={css.statistics}>
-        <h2 className={css.title}> title </h2>
+        <h2 className={css.title}> {title} </h2>
 
             <ul className={css.statList}>
             
