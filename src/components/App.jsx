8645cda7b26fc6c20e60ad641026c538ea807bdd
Anton0694 <91780319+Anton0694@users.noreply.github.com>
2023-02-01 23:50:13 +0200

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -37,7 +37,7 @@ export const App = () => {
         <Statistics stats={data} />
       
       <FriendList friends={friends} />
-      <TransactionHistory items={transactions} />;
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
