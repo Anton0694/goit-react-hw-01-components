09928f4158203030333a6e8e0a095e2060b70953
Anton0694 <91780319+Anton0694@users.noreply.github.com>
2023-02-02 14:03:22 +0200

    fix css

--- src/components/FriendList/FriendList.styled.jsx
+++ src/components/FriendList/FriendList.styled.jsx
@@ -2,17 +2,19 @@ import styled from '@emotion/styled'
 
 export const Status = styled.span`
     display: flex;
-    outline: 1px solid black;
-    width: 30px;
-    height: 30px;
+   margin-top: 60px;
+   margin-left: 30px;
+    width: 50px;
+    height: 50px;
     border-radius: 100%;
+    position: absolut;
     top: 50%;
 
     background-color: ${props => {
     switch (props.isOnline) {
-        case 'true':
+        case true:
             return 'green';
-        case 'false':
+        case false:
             return 'red';
         default:
             return 'grey';
